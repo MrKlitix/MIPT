284663e3cee868fa1243d4e1a7c77c3d40b29b09
Тимур Саяпов <tima_bloger@mail.ru>
2024-12-19 19:46:51 +0500

    Изменение структуры

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ echo "Запуск сборки Docker-контейнера..."
 docker-compose up -d --build
 
 echo "Проверка базы данных..."
-docker exec -it $(docker ps -qf "name=web") python -c "from db import start_database; start_database()"
+docker exec -it $(docker ps -qf "name=web") python -c "from service.db import start_database; start_database()"
 
 echo "Сервис успешно запущен! Откройте http://localhost:5000 в браузере."
